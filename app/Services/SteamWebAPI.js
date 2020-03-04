ca091365539d88d6b4bf558542118ba91504c16a
Nealll68 <hugoducry@gmail.com>
2020-03-04 17:08:31 +0100

    switch to axios

--- app/Services/SteamWebAPI.js
+++ app/Services/SteamWebAPI.js
@@ -1,26 +1,22 @@
 'use strict'
 
-const Helpers = use('Helpers')
-const Request = Helpers.promisify(require('request'))
+const axios = require('axios')
+const qs = require('querystring')
 
 class SteamWebAPI {
     async getFileDetails (workshopID) {
         try {
-            const options = {
-                'method': 'POST',
-                'url': 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1',
-                'headers': {
+            const response = await axios.post('https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1', qs.stringify({
+                'itemcount': 1,
+                'publishedfileids[0]': workshopID
+            }), 
+            {
+                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                form: {
-                    'itemcount': '1',
-                    'publishedfileids[0]': `${workshopID}`
                 }
-            }
-
-            const details = await Request(options)
-
-            return JSON.parse(details.body)
+            })
+            
+            return response.data.response.publishedfiledetails[0]
         } catch (ex) {
             throw ex
         }
@@ -28,44 +24,29 @@ class SteamWebAPI {
 
     async getCollectionDetails (collectionID) {
         try {
-            const collectionOptions = {
-                'method': 'POST',
-                'url': 'https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1',
-                'headers': {
+            const collectionDetails = await axios.post('https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1', qs.stringify({
+                'collectioncount': 1,
+                'publishedfileids[0]': collectionID
+            }), 
+            {
+                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                form: {
-                    'collectioncount': '1',
-                    'publishedfileids[0]': `${collectionID}`
                 }
-            }
+            })
 
-            const steamResponse = await Request(collectionOptions)
-            const collectionDetails = JSON.parse(steamResponse.body)
-            const collectionFilesIds = collectionDetails.response.collectiondetails[0].children.map(element => element.publishedfileid)
-
+            const collectionFilesIds = collectionDetails.data.response.collectiondetails[0].children.map(element => element.publishedfileid)
+            
             let fileDetails = []
             for (const fileId of collectionFilesIds) {                
-                const details = await Request({
-                    'method': 'POST',
-                    'url': 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1',
-                    'headers': {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    form: {
-                        'itemcount': '1',
-                        'publishedfileids[0]': `${fileId}`
-                    }
-                })
+                const details = await this.getFileDetails(fileId)
 
-                const body = JSON.parse(details.body)
                 fileDetails.push({
-                    publishedfileid: body.response.publishedfiledetails[0].publishedfileid,
-                    title: body.response.publishedfiledetails[0].title,
-                    preview_url: body.response.publishedfiledetails[0].preview_url,
-                    time_created: body.response.publishedfiledetails[0].time_created,
-                    time_updated: body.response.publishedfiledetails[0].time_updated,
-                    file_size: body.response.publishedfiledetails[0].file_size
+                    publishedfileid: details.publishedfileid,
+                    title: details.title,
+                    preview_url: details.preview_url,
+                    time_created: details.time_created,
+                    time_updated: details.time_updated,
+                    file_size: details.file_size
                 })
             }
 

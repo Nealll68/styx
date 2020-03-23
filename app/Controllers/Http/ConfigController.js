c6cdb4d1d4c4e148cc2e563fa923f0ea22cf7e28
Nealll68 <hugoducry@gmail.com>
2020-03-23 18:28:25 +0100

    check steamcmd.exe only if path is definde

--- app/Controllers/Http/ConfigController.js
+++ app/Controllers/Http/ConfigController.js
@@ -8,29 +8,27 @@ const Config = use('App/Models/Config')
 const SteamCMDFolderUnavailablePath = use('App/Exceptions/SteamCMDFolderUnavailablePathException')
 
 class ConfigController {
+  async index() {
+    return await Config.first()
+  }
 
-    async index () {
-        return await Config.first()
+  async update({ request, response }) {
+    const data = request.all()
+
+    try {
+      if (data.steamcmd_path && !fs.existsSync(path.join(data.steamcmd_path, 'steamcmd.exe'))) {
+        throw new SteamCMDFolderUnavailablePath()
+      }
+
+      const config = await Config.first()
+      config.merge(data)
+      await config.save()
+
+      return config
+    } catch (ex) {
+      return response.status(ex.status).send(ex.code)
     }
-
-    async update ({ request, response }) {
-        const data = request.all()
-        
-        try {
-            if (!(fs.existsSync(path.join(data.steamcmd_path, 'steamcmd.exe')))) {
-                throw new SteamCMDFolderUnavailablePath()
-            }
-
-            const config = await Config.first()
-            config.merge(data)            
-            await config.save()
-
-            return config
-        } catch (ex) {
-            return response.status(ex.status).send(ex.code)
-        }
-    }
-    
+  }
 }
 
 module.exports = ConfigController

b0b2abd37dd57d05d52f1f0a466ed62e7b9dfeb3
Nealll68 <hugoducry@gmail.com>
2020-03-26 15:01:53 +0100

    use v-banner and responsivity

--- resources/components/PanelHeader.vue
+++ resources/components/PanelHeader.vue
@@ -1,70 +1,40 @@
 <template>
-<div>
-<v-toolbar
-  flat
-  short
-  class="hidden-sm-and-down"
->
-  <v-spacer></v-spacer>
-
-  <v-btn
-    color="tertiary"
-    text
-    :href="helpLink"
-    target="_blank"
-  >
-    <v-icon left>mdi-help-circle</v-icon>{{ $t('common.help') }}
-  </v-btn>
-
-  <v-btn
-    color="quaternary"
-    text
-    :loading="loadingReset"
-    @click="$emit('reset')"	
-  >
-    <v-icon left>mdi-alert-circle</v-icon>{{ $t('common.reset') }}			
-  </v-btn>
-
-  <v-btn
-    color="primary"
-    text
-    :loading="loadingUpdate"
-    @click="$emit('save')"
-  >
-    <v-icon left>mdi-content-save</v-icon>{{ $t('common.save') }}
-  </v-btn>
-</v-toolbar>
-
-<v-bottom-navigation 
+<v-banner
   app
-  grow
-  class="hidden-md-and-up"
+  sticky
 >
-  <v-btn
-    :href="helpLink"
-    target="_blank"
-  >
-    <span>{{ $t('common.help') }}</span>
-    <v-icon>mdi-help-circle</v-icon>
-  </v-btn>
+  <template v-slot:actions>
+    <v-btn
+      :icon="$vuetify.breakpoint.xsOnly"
+      color="tertiary"
+      text
+      :href="helpLink"
+      target="_blank"
+    >
+      <v-icon :left="$vuetify.breakpoint.smAndUp">mdi-help-circle</v-icon>{{ $vuetify.breakpoint.xsOnly ? '' : $t('common.help') }}
+    </v-btn>
 
-  <v-btn
-    :loading="loadingReset"
-    @click="$emit('reset')"	
-  >
-    <span>{{ $t('common.reset') }}</span>
-    <v-icon>mdi-alert-circle</v-icon>
-  </v-btn>
+    <v-btn
+      :icon="$vuetify.breakpoint.xsOnly"
+      color="quaternary"
+      text
+      :loading="loadingReset"
+      @click="$emit('reset')"	
+    >
+      <v-icon :left="$vuetify.breakpoint.smAndUp">mdi-alert-circle</v-icon>{{ $vuetify.breakpoint.xsOnly ? '' : $t('common.reset') }}			
+    </v-btn>
 
-  <v-btn
-    :loading="loadingUpdate"
-    @click="$emit('save')"
-  >
-    <span>{{ $t('common.save') }}</span>
-    <v-icon>mdi-content-save</v-icon>
-  </v-btn>
-</v-bottom-navigation>
-</div>
+    <v-btn
+      :icon="$vuetify.breakpoint.xsOnly"
+      color="primary"
+      text
+      :loading="loadingUpdate"
+      @click="$emit('save')"
+    >
+      <v-icon :left="$vuetify.breakpoint.smAndUp">mdi-content-save</v-icon>{{ $vuetify.breakpoint.xsOnly ? '' : $t('common.save') }}
+    </v-btn>
+  </template>
+</v-banner>
 </template>
 
 <script>

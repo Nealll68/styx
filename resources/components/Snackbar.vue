88f57c02972ed8393a0c0c476144160e7d296773
Nealll68 <hugoducry@gmail.com>
2020-04-09 14:14:45 +0200

    Create new this.$snackbar plugin

---
+++ resources/components/Snackbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-snackbar
+    :value="true"
+    :timeout="6000"
+    top
+  >
+    <v-icon
+      v-if="type === 'info'"
+      color="info"
+    >mdi-alert-circle</v-icon>
+
+    <v-icon
+      v-if="type === 'error'"
+      color="error"
+    >mdi-alert</v-icon>
+
+    <v-icon
+      v-if="type === 'warning'"
+      color="warning"
+    >mdi-exclamation-thick</v-icon>
+
+    <v-icon
+      v-if="type === 'success'"
+      color="success"
+    >mdi-check</v-icon>
+
+    {{ message }}
+    
+    <v-btn
+      icon
+      color="white"
+      @click="close"
+    >
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: ['type', 'message'],
+
+  methods: {
+    close () {
+      this.$destroy()
+    }
+  }
+}
+</script>

a02ea43865f4db31459955ba41253c425986f50a
Nealll68 <hugoducry@gmail.com>
2020-04-10 14:31:41 +0200

    Add left to icon

--- resources/components/Snackbar.vue
+++ resources/components/Snackbar.vue
@@ -7,21 +7,25 @@
     <v-icon
       v-if="type === 'info'"
       color="info"
+      left
     >mdi-alert-circle</v-icon>
 
     <v-icon
       v-if="type === 'error'"
       color="error"
+      left
     >mdi-alert</v-icon>
 
     <v-icon
       v-if="type === 'warning'"
       color="warning"
+      left
     >mdi-exclamation-thick</v-icon>
 
     <v-icon
       v-if="type === 'success'"
       color="success"
+      left
     >mdi-check</v-icon>
 
     {{ message }}

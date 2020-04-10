45eba2d7b4258c79384fc85e93e15bb0ec0211f1
Nealll68 <hugoducry@gmail.com>
2020-04-10 15:49:00 +0200

    inject snackbar component

--- resources/plugins/snackbar.client.js
+++ resources/plugins/snackbar.client.js
@@ -1,29 +1,20 @@
 import Vue from 'vue'
 import Snackbar from '@/components/Snackbar'
 
-function Install (Vue, options) {
-    const vuetify = options.vuetify
-    delete options.vuetify
+export default ({ app }, inject) => {
+  inject('snackbar', ({ type, message }) => {
+    const ComponentClass = Vue.extend(Object.assign({ vuetify: app.vuetify }, Snackbar))
+    const container = document.querySelector('[data-app=true]') || document.body
 
-    function createSnackbar ({ type, message }) {
-        const ComponentClass = Vue.extend(Object.assign({ vuetify }, Snackbar))
-        const container = document.querySelector('[data-app=true]') || document.body
-
-        const instance = new ComponentClass({
-            propsData: { 
-                type,
-                message
-            },
-            destroyed: () => {
-                container.removeChild(instance.$el)
-            }
-        })
-        container.appendChild(instance.$mount().$el)
-    }
-
-    Vue.prototype.$snackbar = createSnackbar
-}
-
-export default ({ app }) => {
-    Vue.use(Install, { vuetify: app.vuetify })
+    const instance = new ComponentClass({
+        propsData: { 
+            type,
+            message
+        },
+        destroyed: () => {
+            container.removeChild(instance.$el)
+        }
+    })
+    container.appendChild(instance.$mount().$el)
+  })
 }

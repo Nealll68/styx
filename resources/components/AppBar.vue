fd20262016e89f46232bce071b19d3a323820755
Nealll68 <hugoducry@gmail.com>
2020-05-15 14:42:49 +0200

    Rework appbar

--- resources/components/AppBar.vue
+++ resources/components/AppBar.vue
@@ -1,78 +1,183 @@
 <template>
 <div>
-  <v-navigation-drawer
-    v-model="navDrawer" 
+  <download-info-bar />
+  
+  <v-app-bar
+    v-if="$vuetify.breakpoint.mdAndUp"
     app
-    mobile-break-point="960"
-    :mini-variant="$vuetify.breakpoint.md || $vuetify.breakpoint.lg"
-  >
-    <div class="d-flex justify-center my-5">
-      <Logo width="50px" height="50px" />
-    </div>
-
-    <v-list>      
-      <v-list-item 
-        v-for="item in items" 
-        :key="item.name"
-        nuxt
-        :to="item.link"
-        :exact="item.link === '/'"
-        color="primary"
-      >
-        <v-list-item-icon>
-          <v-badge
-            :value="item.link === '/settings' && !$store.state.config.a3ServerPath"
-            icon="mdi-exclamation"
-            color="error"
-            overlap
-          >
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-badge>          
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.name }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-
-    <template v-slot:append>
-      <v-list>
-        <v-list-item
-          nuxt
-          to="/user"
-          exact
-          color="primary"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-account-cog</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ $t('menu.myAccount') }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item
-          @click="logout()"
-          color="error"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-logout</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ $t('menu.logout') }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </template>
-  </v-navigation-drawer>
-
-  <v-app-bar 
-    app 
     elevate-on-scroll
-    dense
+    fixed
+  >
+    <v-container fluid>
+      <v-row align="center">
+        <v-col md="4">
+          <Logo width="50px" height="50px" />
+        </v-col>
+
+        <v-col 
+          md="4" 
+          class="d-flex justify-center"
+        >
+           <v-tooltip 
+            v-for="link in links"
+            :key="link.url"
+            bottom
+           >
+            <template v-slot:activator="{ on }">
+              <v-btn 
+                v-on="on"
+                :to="link.url"
+                nuxt
+                text
+                class="mx-1"
+                exact-active-class="appbar-active-link"
+              >
+                <v-badge
+                  :value="link.url === '/settings' && !$store.state.config.a3ServerPath"
+                  icon="mdi-exclamation"
+                  color="error"
+                  overlap
+                >
+                  <v-icon>{{ link.icon }}</v-icon>
+                </v-badge>
+              </v-btn>
+            </template>
+            <span>{{ link.name }}</span>
+          </v-tooltip>          
+        </v-col>
+
+        <v-col 
+          md="4"
+          class="d-flex justify-end"
+        >
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn 
+                v-on="on"
+                to="/user"
+                nuxt
+                text
+                class="mx-1"
+                exact-active-class="appbar-active-link"
+              >
+                <v-icon>mdi-account-cog</v-icon>
+              </v-btn>
+            </template>
+            <span>{{ $t('menu.myAccount') }}</span>
+          </v-tooltip>
+
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn 
+                v-on="on"
+                @click="logout()"
+                text
+                class="mx-1"
+                exact-active-class="appbar-active-link"
+              >
+                <v-icon>mdi-logout</v-icon>
+              </v-btn>
+            </template>
+            <span>{{ $t('menu.logout') }}</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <template v-slot:extension> 
+      <v-chip 
+        small 
+        label 
+        outlined
+        class="mx-1 hidden-sm-and-down"
+      >
+        <v-icon 
+          small 
+          left
+        >mdi-account</v-icon> {{ $auth.user.username }} - {{ privileges[$auth.user.privilege] }}            
+      </v-chip>
+
+      <v-progress-circular
+        v-if="measure"
+        :value="measure.cpu_usage"
+        color="tertiary"
+        class="mx-2 hidden-xs-only"
+      >
+        <v-icon 
+          small
+          color="tertiary"
+        >mdi-chip</v-icon>
+      </v-progress-circular>
+
+      <v-progress-circular
+        v-if="measure"
+        :value="measure.mem_used"
+        color="quaternary"
+        class="mx-2 hidden-xs-only"
+      >
+        <v-icon 
+          small
+          color="quaternary"
+        >mdi-memory</v-icon>
+      </v-progress-circular>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :icon="$vuetify.breakpoint.smAndDown" 
+        text 
+        color="success"
+        @click="startA3Server()" 
+        :disabled="$store.state.downloadInfo.type === 'updateServer' || $store.state.a3Server.isStarted || !$store.state.config.a3ServerPath"
+      >
+        <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-play</v-icon>{{ $vuetify.breakpoint.smAndDown ? '' : $t('serverState.start') }}
+      </v-btn>
+
+      <v-btn
+        :icon="$vuetify.breakpoint.smAndDown" 
+        text 
+        color="warning"
+        @click="restartA3Server()" 
+        :disabled="$store.state.downloadInfo.type === 'updateServer' || !$store.state.a3Server.isStarted || !$store.state.config.a3ServerPath"
+      >
+        <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-restart</v-icon>{{ $vuetify.breakpoint.smAndDown ? '' : $t('serverState.restart') }}
+      </v-btn>
+
+      <v-btn
+        :icon="$vuetify.breakpoint.smAndDown" 
+        text 
+        color="error"
+        @click="stopA3Server()" 
+        :disabled="$store.state.downloadInfo.type === 'updateServer' || !$store.state.a3Server.isStarted || !$store.state.config.a3ServerPath"
+      >
+        <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-stop</v-icon>{{ $vuetify.breakpoint.smAndDown ? '' : $t('serverState.stop') }}
+      </v-btn>
+      
+      <v-tooltip 
+        v-if="$auth.user.privilege === 1 && $store.state.config.steamCmdPath"
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            :loading="loadingUpdate"
+            v-on="on"
+            @click="updateServer()"
+            :disabled="$store.state.downloadInfo.type === 'updateServer' || !$store.state.config.a3ServerPath"
+          >
+            <v-icon>mdi-update</v-icon>
+          </v-btn>
+        </template>
+        {{ $t('menu.updateServer') }}
+      </v-tooltip>
+    </template> 
+  </v-app-bar>
+
+  <v-app-bar
+    v-else
+    app
+    elevate-on-scroll
+    fixed
   >
     <v-btn
       icon
@@ -82,47 +187,7 @@
       <v-icon>mdi-menu</v-icon>
     </v-btn>
 
-    
-    <v-menu 
-      bottom
-      right
-      transition="slide-y-transition"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          :icon="$vuetify.breakpoint.smAndDown"
-          :small="$vuetify.breakpoint.mdAndUp"
-          text          
-          v-on="on"          
-        >
-          <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-translate</v-icon>
-          {{ $vuetify.breakpoint.smAndDown ? '' : activatedLocale }}
-          <v-icon 
-            v-show="$vuetify.breakpoint.mdAndUp"
-            right 
-          >mdi-menu-down</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list 
-        nav 
-        dense
-      >
-        <v-list-item
-          v-for="locale in $store.state.i18n.locales"
-          :key="locale.code"
-          @click="switchLocale(locale.code)"
-        >
-          <v-list-item-content>
-            <v-list-item-title>{{ locale.name }}</v-list-item-title>
-          </v-list-item-content>
-
-          <v-list-item-icon v-if="locale.code === $store.state.i18n.locale">
-            <v-icon color="primary">mdi-check</v-icon>
-          </v-list-item-icon>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <Logo width="50px" height="50px" />
 
     <v-chip 
       small 
@@ -211,7 +276,69 @@
     </v-tooltip>
   </v-app-bar>
 
-  <download-info-bar />
+  <v-navigation-drawer
+    v-if="$vuetify.breakpoint.smAndDown"
+    v-model="navDrawer" 
+    app
+  >
+    <v-list>      
+      <v-list-item 
+        v-for="link in links" 
+        :key="link.url"
+        nuxt
+        :to="link.url"
+        :exact="link.url === '/'"
+        color="primary"
+      >
+        <v-list-item-icon>
+          <v-badge
+            :value="link.url === '/settings' && !$store.state.config.a3ServerPath"
+            icon="mdi-exclamation"
+            color="error"
+            overlap
+          >
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-badge>          
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ link.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <v-list>
+        <v-list-item
+          nuxt
+          to="/user"
+          exact
+          color="primary"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-account-cog</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ $t('menu.myAccount') }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          @click="logout()"
+          color="error"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ $t('menu.logout') }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </template>
+  </v-navigation-drawer>
 </div>
 </template>
 
@@ -232,12 +359,12 @@ export default {
       loadingUpdate: false,
       navDrawer: null,
       a3ServerWs: null,
-      items: [
-        { link: '/', name: this.$t('menu.index'), icon: 'mdi-view-dashboard' },
-        { link: '/profile', name: this.$t('menu.profiles'), icon: 'mdi-folder-cog' },
-        { link: '/mods', name: this.$t('menu.mods'), icon: 'mdi-toy-brick' },
-        { link: '/mission', name: this.$t('menu.missions'), icon: 'mdi-target' },        
-        { link: '/logs', name: this.$t('menu.logs'), icon: 'mdi-post' }
+      links: [
+        { url: '/', name: this.$t('menu.index'), icon: 'mdi-view-dashboard' },
+        { url: '/profile', name: this.$t('menu.profiles'), icon: 'mdi-folder-cog' },
+        { url: '/mods', name: this.$t('menu.mods'), icon: 'mdi-toy-brick' },
+        { url: '/mission', name: this.$t('menu.missions'), icon: 'mdi-target' },        
+        { url: '/logs', name: this.$t('menu.logs'), icon: 'mdi-post' }
       ],
       privileges: [this.$t('privileges.sergeant'), this.$t('privileges.commander')]
     }
@@ -257,8 +384,8 @@ export default {
 
   created () {
     if (this.$auth.user.privilege === 1) {
-      this.items.push({ link: '/users', name: this.$t('menu.users'), icon: 'mdi-account-group' })
-      this.items.push({ link: '/settings', name: this.$t('menu.settings'), icon: 'mdi-cogs' })    
+      this.links.push({ url: '/users', name: this.$t('menu.users'), icon: 'mdi-account-group' })
+      this.links.push({ url: '/settings', name: this.$t('menu.settings'), icon: 'mdi-cogs' })    
     }
   },
 

aec4dab7fd0940f16ce184f80ac9250d5717746c
Nealll68 <hugoducry@gmail.com>
2020-05-18 17:53:03 +0200

    Remove nuxt-clipboard2

--- resources/pages/profile/_name/config.vue
+++ resources/pages/profile/_name/config.vue
@@ -133,7 +133,11 @@ export default {
   
   async mounted () {
     this.config = await this.$axios.$get(`server/config/${this.$route.params.name}`)
-    this.missions = await this.$axios.$get('server/mission')
+    //this.missions = await this.$axios.$get('server/mission')
+    this.missions = [
+      { id: 1, filename: 'test.altis.pbo' },
+      { id: 2, filename: 'test5.stratis.pbo' }
+    ]
     this.loading = false
   },
 
